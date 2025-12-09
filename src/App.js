6852df7ed3d1d1de6759ee2ac9e3bb2b07be112f
Hector <hectorjoshlopez@gmail.com>
2025-12-09 13:41:28 +0800

    Deploy public visitor registration only

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import VisitorPublicPage from "./pages/VisitorPublicPage";
 function App() {
   const [visitors, setVisitors] = useState([]);
 
-  // Add visitor to main system
   const addVisitor = (visitor) => {
     setVisitors((prev) => [...prev, visitor]);
-    console.log("Visitor added to main system:", visitor);
   };
 
   return <VisitorPublicPage addVisitor={addVisitor} />;
